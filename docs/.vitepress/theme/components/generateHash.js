ead63688fe771ab48984304caf8cb2affd103e77
takuyasec <qiankunhzj@gmail.com>
2024-10-04 01:08:41 +0800

    mac_20241003_12

--- docs/.vitepress/theme/components/generateHash.js
+++ docs/.vitepress/theme/components/generateHash.js
@@ -6,8 +6,8 @@ const password = 'Huang@1020.p@ssw0d';
 // 生成一个随机盐
 const salt = CryptoJS.lib.WordArray.random(128/8).toString();
 
-// 生成哈希值
-const hash = CryptoJS.SHA256(password + salt).toString();
+// 生成哈希值（使用 HMAC-SHA256）
+const hash = CryptoJS.HmacSHA256(password, salt).toString();
 
-console.log(`VITE_PAGW_PASSWORD_HASH=${hash}`);
+console.log(`VITE_PAGE_PASSWORD_HASH=${hash}`);
 console.log(`VITE_PAGE_PASSWORD_SALT=${salt}`);
